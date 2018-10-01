73000b1ac9d46591d9accadc63e777feab5b976d
Martin Fiedler <keyj@emphy.de>
2018-10-01 14:05:26 +0200

    major RasPi installation overhaul
    - a third directory is now configured
      for configuration and state data
    - the location of the data directories
      (root or boot partition) is now detected
      automatically: whichever partition is larger
      will be used
    - proper configuration file with comments
    - 'jukebox' pseudo-user now doesn't even need a
      proper home directory any longer

--- README.md
+++ README.md
@@ -3,27 +3,55 @@
 `kjukebox` plays videos in a specified directory in fullscreen using some standard media player application (MPV, VLC, MPC-HC, or OMXPlayer on Raspberry Pi). Its main purpose is automated playback in random order, but playlists can also be specified manuelly using the built-in web interface. A text-based announcement screen is shown between videos.
 
 
+
+
+
 ## Raspberry Pi installation
 
 kjukebox can be used as a fully-automated jukebox system on Raspberry Pi devices (any generation), including a web interface and even (optional) Samba access to the data directory. To set this up, do the following:
 
-1. Use the rpi_sdcard_setup.sh script (on a Linux machine) to prepare an SD card for the RasPi. This will download and install the latest Raspbian Lite, but it will not just `dd` the image onto the card, but split the partitions in a way that the FAT32 boot partition is larger than usual. The reason is that this is where the video files will be stored; this has been done so that it's possible to manage the videos on the jukebox using any old Windows box.
 
-2. Boot the RasPi from the SD card. You may want to use `sudo raspi-config` to configure the keyboard, timezone and primary user's password before doing anything else.
+#### SD card preparation
 
-3. Copy `kjukebox_install.sh` onto the RasPi and run it (as root or via `sudo`). This will guide you through the remaining installation process. Or just fetch and run the most recent version directly from GitHub:
+##### Variant 1: Use a standard SD card image
+
+Download [the latest Raspbian Lite image from the official Website](https://downloads.raspberrypi.org/raspbian_lite_latest) and write it to an SD card as usual, using [Etcher](https://etcher.io/), [Rufus](https://rufus.akeo.ie/), or if you have a Linux machine, just
+```
+wget -qO- https://downloads.raspberrypi.org/raspbian_lite_latest | funzip | dd status=progress of=/dev/mmcblkX/or/sdX/depends_on_your_system
+```
+
+If you prepare the SD card in this way, the videos will be stored on the ext4-formatted root partition of the card.
+
+##### Variant 2: Custom formatting with a large FAT partition
+
+Alternatively, you can use the `rpi_sdcard_setup.sh` script from this repository on a Linux machine. It also installs an official Raspbian Lite image onto an SD card, but it uses a custom partition layout that makes the FAT boot partition larger than the ext4 root partition. Videos and configuration data will then be stored on the FAT partition as well. This has the benefit that video data on the SD card can be managed from a Windows or OS X machine.
+
+
+#### First boot and jukebox installation
+
+Boot the RasPi from the SD card. You may want to use `sudo raspi-config` to configure the keyboard, timezone and primary user's password, and perhaps enable SSH access before doing anything else.
+
+Then, copy `kjukebox_install.sh` onto the RasPi and run it (as root or via `sudo`). This will guide you through the remaining installation process. Or just fetch and run the most recent version directly from GitHub:
 ```
 wget https://raw.githubusercontent.com/kajott/kjukebox/master/kjukebox_install.sh && sudo bash kjukebox_install.sh
 ```
 
-4. Shut down the RasPi and copy video files into the `JukeboxContent` directory in the FAT32 partition on the SD card. The files must use H.264 Baseline, Main or High Profile up to 1080p resolution. Some slight (and safe) overclocking is applied to make 1080p60 work to some extent, but perfect playback is only guaranteed up to 1080p30 or 720p60.
 
-5. Boot the RasPi again. It will automatically start the video show after a few seconds of delay. To inhibit this, press Ctrl+C during booting when the "Jukebox will start in 10 seconds" prompt appears and log in normally on another console (by pressing Alt+F2, for example).
+#### Configuration and content installation
 
-6. Control the video show remotely using the HTTP URL that is shown between videos.
+Review the configuration file (`/srv/jukebox/config/config.txt`, or `JukeboxConfig/config.txt` on the FAT partition if the SD card was set up appropriately), and copy video files into `/srv/jukebox/content` or the `JukeboxContent` directory of the SD card's FAT partition (if used). If you enabled Samba write access during jukebox instalation, videos can also be copied to the card over the network.
+
+Video files must use H.264 Baseline, Main or High Profile up to 1080p resolution. Some slight (and safe) overclocking is applied to make 1080p60 work to some extent, but perfect playback is only guaranteed up to 1080p30 or 720p60.
+
+Finally, reboot the RasPi. It will automatically start the video show after a few seconds of delay. To inhibit this, press Ctrl+C during booting when the "Jukebox will start in 10 seconds" prompt appears and log in normally on another console (by pressing Alt+F2, for example).
+
+You can control the video show remotely using the HTTP URL that is shown between videos.
 
 
-### Downloading videos
+
+
+
+## Downloading videos
 
 If you want to download video files from YouTube or other streaming sites to be used with a Raspberry Pi-based video jukebox, here's an appropriate configuration for [youtube-dl](http://rg3.github.io/youtube-dl/):
 
--- kjukebox_install.sh
+++ kjukebox_install.sh
@@ -40,23 +40,37 @@ loc="$(dirname $0)/kjukebox.py"
 if [ -x /usr/local/bin/kjukebox ] ; then
     echo -n "kjukebox is already installed, upgrade to latest version? [y/N] "
     read dl
-    if [ "$dl" != "y" ] ; then
-        echo "Keeping currently installed version."
-        dl=""
+    if [ "$dl" == "y" ] ; then
+        dl="dl"
+    elif [ -f "$loc" -a "$loc" -nt /usr/local/bin/kjukebox ] ; then
+        echo -n "$(readlink -f "$loc") is newer than the installed version; upgrade to that? [Y/n] "
+        read dl
+        if [ "$dl" != "n" ] ; then
+            dl="local"
+        else
+            dl="keep"
+        fi
+    else
+        dl="keep"
     fi
 elif [ -e "$loc" ] ; then
     echo -n "Install latest version instead of the one in $(readlink -f "$loc")? [y/N] "
     read dl
-    if [ "$dl" != "y" ] ; then
-        echo "Installing local copy."
-        ( set -x ; install -m 755 "$loc" /usr/local/bin/kjukebox )
-        dl=""
+    if [ "$dl" == "y" ] ; then
+        dl="dl"
+    else
+        dl="local"
     fi
 else
     # no binary found at all, need to download a new one anyway
-    dl="y"
+    dl="dl"
 fi
-if [ -n "$dl" ] ; then
+if [ "$dl" == "keep" ] ; then
+    echo "Keeping installed version."
+elif [ "$dl" == "local" ] ; then
+    echo "Installing local copy."
+    ( set -x ; install -m 755 "$loc" /usr/local/bin/kjukebox )
+elif [ "$dl" == "dl" ] ; then
     echo "Downloading latest upstream version of kjukebox ..."
     ( set -x ; wget -nv -O /usr/local/bin/kjukebox $UPSTREAM_URL )
     ( set -x ; chmod +x /usr/local/bin/kjukebox )
@@ -84,7 +98,7 @@ echo "jukebox is possible, but also access to the audio/video files."
 echo "Please select the type of Samba installation you want:"
 echo " - N = no Samba at all"
 echo " - R = public read-only access to jukebox contents"
-echo " - W = public read and write access to jukebox contents"
+echo " - W = public read and write access to jukebox contents and configuration"
 echo " - I = public read-only access to jukebox contents,"
 echo "       and read/write access to an additional \"incoming\" directory"
 
@@ -111,18 +125,21 @@ case "$(echo "$smbmode" | tr [[:upper:]] [[:lower:]])" in
         echo "Selected mode: read-only"
         smb_en="enable"
         smb_content_wr="no"
+        smb_config=""
         smb_incoming=""
         ;;
     w*)
         echo "Selected mode: read-write"
         smb_en="enable"
         smb_content_wr="yes"
+        smb_config="yes"
         smb_incoming=""
         ;;
     i*)
         echo "Selected mode: separate incoming"
         smb_en="enable"
         smb_content_wr="no"
+        smb_config=""
         smb_incoming="yes"
         ;;
     *)
@@ -134,20 +151,118 @@ echo
 
 ###############################################################################
 
-echo "##### configuring shutdown command"
-echo "A (possibly secret) web interface command can be defined that shuts down"
-echo "the system in a clean manner. Its URL is http://<deviceIP>/<command>."
-echo
-quitdefault=$(grep '^QUITCMD=' /usr/local/bin/autostart_jukebox 2>/dev/null | cut -d= -f2)
-[ -z "$quitdefault" ] && quitdefault=shutdown
-echo -n "Shutdown command? [default: $quitdefault] "
-read quitcmd
-[ -z "$quitcmd" ] && quitcmd=$quitdefault
+echo "##### package installation"
+pkgfail=""
+which omxplayer.bin >/dev/null || pkgfail="$pkgfail omxplayer"
+if [ "$smb_en" == "enable" ] ; then
+    which samba >/dev/null || pkgfail="$pkgfail samba"
+fi
+if [ -n "$pkgfail" ] ; then
+    echo "Installing required packages:$pkgfail"
+    ( set -x ; apt update )
+    ( set -x ; apt -y install $pkgfail )
+else
+    echo "All required packages are already installed."
+fi
 echo
 
-echo "##### installing autostart script in /usr/local/bin/autostart_jukebox"
+###############################################################################
+
+echo "##### creating jukebox user"
+if getent passwd jukebox >/dev/null ; then
+    echo "User already exists."
+else
+    ( set -x ; useradd -U -G audio,video -d /srv/jukebox/config -M -s /usr/local/bin/autostart_jukebox jukebox )
+fi
+echo
+
+###############################################################################
+
+echo "##### creating directories and mount points"
+bootspace=$(df --output=avail /boot | grep -E '^[0-9]+$')
+rootspace=$(df --output=avail / | grep -E '^[0-9]+$')
+if [ $bootspace -gt $rootspace ] ; then
+    data="boot"
+    echo "Boot (FAT) partition is larger than root (ext4) partition,"
+    echo "installing directories for video and configuration data on boot (FAT)."
+    ( set -x ; mkdir -p /boot/{JukeboxContent,JukeboxConfig,incoming} /srv/jukebox/{content,config,incoming} )
+    ( set -x ; sudo sed -ri "s:(/boot.*defaults)[^ \t]*:\1,gid=$(id -g jukebox),umask=002,iocharset=utf8:" /etc/fstab )
+    grep /srv/jukebox/content /etc/fstab &>/dev/null || ( set -x ; echo -e "/boot/JukeboxContent\t/srv/jukebox/content\tnone\tbind\t0\t3" >>/etc/fstab )
+    grep /srv/jukebox/config /etc/fstab &>/dev/null || ( set -x ; echo -e "/boot/JukeboxConfig\t/srv/jukebox/config\tnone\tbind\t0\t3" >>/etc/fstab )
+    grep /srv/jukebox/incoming /etc/fstab &>/dev/null || ( set -x ; echo -e "/boot/incoming\t/srv/jukebox/incoming\tnone\tbind\t0\t3" >>/etc/fstab )
+    ( set -x ; mount -a )
+else
+    data="root"
+    echo "Root (ext4) partition is larger than boot (FAT) partition,"
+    echo "installing directories for video and configuration data on root (ext4)."
+    umount /srv/jukebox/{content,config,incoming} 2>/dev/null || true
+    sed -ri 's:.*/srv/jukebox/.*::' /etc/fstab
+    ( set -x ; mkdir -p /srv/jukebox/{content,config,incoming} )
+    ( set -x ; chown jukebox:jukebox /srv/jukebox/{content,config,incoming} || true )
+fi
+echo
+
+###############################################################################
+
+echo "##### installing configuration file and autostart script"
+if [ -f /srv/jukebox/config/config.txt ] ; then
+    echo "Configuration file exists already, not overwriting it."
+else
+    echo "Installing new configuration file."
+    sed 's:$:$:' <<EOF | tr '$' '\r' >/srv/jukebox/config/config.txt
+# This is the configuration file for kjukebox.
+
+# This file contains the command-line parameters for kjukebox,
+# with which many useful things can be configured.
+# Empty lines and lines beginning with '#' are ignored.
+
+# Set the port for the web interface to 80 (normal HTTP).
+--port 80
+
+# Set the name of a log file; mostly useful for debugging.
+#--logfile kjukebox.log
+
+# Set the name of the playlist, history and play counts file.
+--statefile state.txt
+
+# Enable automatic saving of the state file after each played track.
+# This may make the system a little more robust against random power loss,
+# but it slightly increases wear on the SD card.
+--autosave
+
+# Automatically rescan the jukebox content directory for new or deleted files
+# after each played track. Only really useful when content is actually added
+# or removed while the system is running (e.g. via Samba), but doesn't do any
+# harm if the content is perfectly static either.
+--autoscan
+
+# Start playback automatically when the system starts up.
+# If disabled, playback needs to be initiated manually in the web interface.
+--autoplay
+
+# Set how many tracks shall be kept in the history across reboots.
+--maxhist 100
+
+# Display a text file instead of the jukebox's IP address between videos.
+# Can also be used to disable the IP address display: just specify an invalid
+# file name, e.g. '--logo -'.
+#--logo logo.txt
+
+# Set (possibly secret) commands for shutting down or rebooting the system.
+# They can be invoked using http://<IP_address>/<command>.
+# Shutdown uses code 99, reboot uses code 77.
+--quitcmd shutdown=99
+--quitcmd reboot=77
+
+# Other options (host name, Samba options) can't be configured in this file;
+# please re-run kjukebox_install.sh to change these options.
+EOF
+    chown jukebox:jukebox /srv/jukebox/config/config.txt 2>/dev/null || true
+fi
+
 cat >/usr/local/bin/autostart_jukebox <<EOF
 #!/bin/bash
+cd /srv/jukebox/config
 
 setfont /usr/share/consolefonts/Uni2-TerminusBold28x14.psf
 setterm --blank 0 --powersave off --powerdown 0
@@ -184,49 +299,18 @@ echo "           ---------------------"
 echo
 echo
 
-QUITCMD=$quitcmd
-kjukebox -p 80 -a -s -r -l jukebox.log -q \$QUITCMD=47 /srv/jukebox/content
-[ \$? == 47 ] && sudo shutdown -P now
+options="\$(tr -d '\r' </srv/jukebox/config/config.txt | sed 's:^#.*::')"
+set -x
+kjukebox \$options /srv/jukebox/content
+code=\$?
+set +x
+[ \$code == 99 ] && sudo shutdown -P now
+[ \$code == 77 ] && sudo shutdown -r now
 
 sync
 killterm
 EOF
 ( set -x ; chmod 755 /usr/local/bin/autostart_jukebox )
-echo
-
-###############################################################################
-
-pkgfail=""
-which omxplayer.bin >/dev/null || pkgfail="$pkgfail omxplayer"
-if [ "$smb_en" == "enable" ] ; then
-    which samba >/dev/null || pkgfail="$pkgfail samba"
-fi
-if [ -n "$pkgfail" ] ; then
-    echo "##### installing required packages:$pkgfail"
-    apt update
-    apt -y install $pkgfail
-else
-    echo "##### all required packages are already installed"
-fi
-echo
-
-###############################################################################
-
-if [ -d /home/jukebox ] ; then
-    echo "##### jukebox user already exists"
-else
-    echo "##### creating jukebox user"
-    useradd -m -U -G audio,video -s /usr/local/bin/autostart_jukebox jukebox
-fi
-echo
-
-###############################################################################
-
-echo "##### creating directories and mount points"
-( set -x ; mkdir -p /boot/{JukeboxContent,incoming} /srv/jukebox/{incoming,content} )
-( set -x ; sudo sed -ri "s:(/boot.*defaults)[^ \t]*:\1,gid=$(id -g jukebox),umask=002,iocharset=utf8:" /etc/fstab )
-grep /srv/jukebox/content /etc/fstab &>/dev/null || ( set -x ; echo -e "/boot/JukeboxContent\t/srv/jukebox/content\tnone\tbind\t0\t3" >>/etc/fstab )
-grep /srv/jukebox/incoming /etc/fstab &>/dev/null || ( set -x ; echo -e "/boot/incoming\t/srv/jukebox/incoming\tnone\tbind\t0\t3" >>/etc/fstab )
 echo
 
 ###############################################################################
@@ -298,6 +382,19 @@ writable = $smb_content_wr
 guest ok = yes
 map read only = no
 EOF
+    if [ -n "$smb_config" ] ; then
+        cat >>/etc/samba/smb.conf <<EOF
+
+[config]
+path = /srv/jukebox/config
+comment = Jukebox Configuration and State
+public = yes
+browseable = yes
+writable = yes
+guest ok = yes
+map read only = no
+EOF
+    fi
     if [ -n "$smb_incoming" ] ; then
         cat >>/etc/samba/smb.conf <<EOF
 
@@ -314,15 +411,23 @@ EOF
 fi
 ( set -x ; systemctl $smb_en smbd.service 2>/dev/null || true )
 ( set -x ; systemctl $smb_en nmbd.service 2>/dev/null || true )
+if [ "$smb_en" == "enable" -a "$smb_content_wr" == "yes" ] ; then
+    ( set -x ; systemctl start smbd.service 2>/dev/null || true )
+    ( set -x ; systemctl start nmbd.service 2>/dev/null || true )
+fi
 echo
 
 ###############################################################################
 
 echo "##### everything done"
-mount -a
 sync
 echo
-echo "Now copy some video files to /srv/jukebox/content"
-echo "or the JukeboxContent directory on the FAT32 boot partition"
-echo "and restart the system."
+echo "Now review the configuration file in /srv/jukebox/config/config.txt,"
+echo "copy some video files to /srv/jukebox/content"
+if [ "$smb_en" == "enable" -a "$smb_content_wr" == "yes" ] ; then
+    echo "(also possible over the network, via Samba)"
+elif [ "$data" == "boot" ] ; then
+    echo "(or the JukeboxContent directory on the FAT boot partition)"
+fi
+echo "and finally restart the system."
 echo
